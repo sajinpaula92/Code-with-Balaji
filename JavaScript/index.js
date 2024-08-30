bdea33eda048989d603c78d16ed1098db3c9353d
Nsuresafe <elearn@ensuresafe.sg>
2024-08-30 08:51:10 +0530

    Javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,119 @@
+
+num1 = 456;
+
+// Before Es6 
+str = 'hello'
+str2= "hello"
+
+// After Es6 
+str3 = `hello ${num1} ` //Template literal
+
+isActive = true;
+isActive = false;
+
+// console.log(isActive)
+// console.log(typeof str3)
+// console.log(typeof isActive)
+// console.log(typeof num1)
+// console.log(typeof num1)
+
+
+// Arithmetic operators 
+
+
+// + , - , * , / , % , ** , ++ , --
+
+a = 5;
+b = 2; // statement
+
+a+b // expression
+// a , b - 2 Operands 
+// + - operator 
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+
+// %  - modulus  
+
+// console.log(a%b) // 40 % 20 - 0
+
+
+// console.log(a**b)
+
+// unary operator - single operand
+// console.log(++a , a++) // a = a+1 , 6 , 7  
+// console.log(--a , a--) // a = a-1
+
+// console.log(a);
+
+// ++a  = pre increment
+// a++ = post increment
+
+
+// Output screen :
+
+// 6 6
+// 6 6
+
+// a = 7
+
+
+
+// String concatination (addition)
+
+// Greeting = "Hello , ";
+// Name = "Balaji";
+// console.log(Greeting + Name )
+
+// id = 456
+// id2 = 456
+// id3 = '567';
+
+// result = id + id2 + Number(id3); // left to right
+// result = id.toString() + id2 + id3; // left to right
+
+//  console.log(result)
+
+
+// IF-Else 
+
+a = 10;
+b = '10';
+
+a > b
+10 > 10 - false
+
+
+result = a + b; // 30
+
+// == and ===
+
+30 == '30' - true
+30 == 30 - true
+
+30 === 30 - true
+30 === '30' - false
+
+
+// 10 <= 10 - true
+// if(a !== b){
+//     console.log("Balaji")
+// } else {
+//     console.log("Sajin")
+// }
+
+// Comparision operators 
+
+// > , < , >= , <= , == , === , != , !== 
+
+
+// age = 28;
+
+// if(age>=18){
+//     console.log("Eligible for voting")
+// }else{
+//     console.log("Not eleigible for voting")
+// }
+
